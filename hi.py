652169cda680e379a65768ece06020ab2e009a81
idk-aarza <aarzak2012@gmail.com>
2025-07-24 08:46:22 +0530

    hi

--- hi.py
+++ hi.py
@@ -1,21 +1,17 @@
 import requests
 
-# Technology category fact endpoint
-url = "https://uselessfacts.jsph.pl/random.json?language=en"
+url = "https://uselessfacts.jsph.pl/category/Science.json?language=en"
 
-
-# Function to fetch and display a random technology-related fact
-def get_random_technology_fact():
+def get_random_science_fact():
     response = requests.get(url)
     if response.status_code == 200:
         fact_data = response.json()
         print(f"Did you know? {fact_data['text']}")
     else:
         print("Failed to fetch fact")
-
-# Main loop to interact with the user
 while True:
-    user_input = input("Press Enter to get a random technology fact or type 'q' to quit... ")
+    user_input = input("Press Enter to get a random science fact or type 'q' to quit... ")
+    
     if user_input.lower() == 'q':
         break
-    get_random_technology_fact()
+    get_random_science_fact()
